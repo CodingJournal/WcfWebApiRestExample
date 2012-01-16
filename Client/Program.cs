ab714ad024f144d85de90799e5b21465c08f3678
Lucas <lucas@x3ro.de>
2012-01-16 01:23:13 +0100

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace Client
+{
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebGet]
+        int Multiply(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped)]
+        bool CreateNewUser(string username, string password);
+    }
+
+    class Program
+    {
+        private const string RestUri = "http://localhost:8080";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var cf = new ChannelFactory<IRestService>(new WebHttpBinding(), RestUri))
+                {
+                    cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
+                    var channel = cf.CreateChannel();
+
+                    Console.WriteLine("Calling Multiply: ");
+                    var result = channel.Multiply(6, 23);
+                    Console.WriteLine("6*23 = {0}", result);
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Calling CreateNewUser (POST): ");
+                    var result1 = channel.CreateNewUser("foobar", "verystrongpasswortnot");
+                    Console.WriteLine("User created? {0}", result1);
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine("Press <ENTER> to terminate");
+                Console.ReadLine();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine("An exception occurred: {0}", e.Message);
+            }
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Server
+{
+    /// <summary>
+    /// Our service contract, this should look familiar to you if you know WCF.
+    /// If you've never used WCF before, you should check out the screen casts
+    /// at http://msdn.microsoft.com/en-us/netframework/dd939784
+    /// </summary>
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebGet]
+        int Multiply(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped)]
+        bool CreateNewUser(string username, string password);
+    }
+
+
+
+    public class RestService : IRestService
+    {
+        public int Multiply(int a, int b)
+        {
+            Console.WriteLine("Multiply received");
+            return a * b;
+        }
+
+        public bool CreateNewUser(string username, string password)
+        {
+            Console.WriteLine("CreateNewUser received");
+            if (username.Length < 3 || password.Length < 8)
+            {
+                return false;
+            }
+
+            // [ Do lots of stuff ]
+
+            return true;
+        }
+    }
+
+
+
+    class Program
+    {
+        private const string RestUri = "http://localhost:8080/";
+
+        static void Main(string[] args)
+        {
+            var host = new WebServiceHost(typeof(RestService), new Uri(RestUri));
+            try
+            {
+                host.AddServiceEndpoint(typeof(IRestService), new WebHttpBinding(), "");
+                host.Open();
+
+                Console.WriteLine("Press <ENTER> to terminate");
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine("An exception occurred: {0}", e.Message);
+                host.Abort();
+            }
+        }
+    }
+}
